5b9df01df5c597c01224a070f5d805d12d12bd85
Danil Kozyavin <57450873+ligmanstark@users.noreply.github.com>
2023-11-16 23:47:15 +0400

    feat:create_private-auth&public-auth&controllers (#7)

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -2,23 +2,30 @@ import { FC } from 'react';
 import { GlobalStyles } from '../styled/GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
-import { publicRoutes } from './routes';
-
+import { publicRoutes, privateRoutes } from './routes';
+import { ProtectedRoute } from '../components/helpers/ProtectedRoute';
 const Application: FC = () => {
-  return (
-    <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            {publicRoutes.map(({ path, component }) => (
-              <Route key={path} path={path} element={component} />
-            ))}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  //для доступа к profile и workout ставим true
+	const auth = false;
+	return (
+		<>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Layout />}>
+						{publicRoutes.map(({ path, component }) => (
+							<Route key={path} path={path} element={component} />
+						))}
+						<Route element={<ProtectedRoute auth={auth} />}>
+							{privateRoutes.map(({ path, component }) => (
+								<Route key={path} path={path} element={component} />
+							))}
+						</Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 };
 
 export default Application;
