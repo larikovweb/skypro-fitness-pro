61e7a5dbe9480eeb768bdda02393c8e2c7665a35
DARIA S <DARIA@EXAMPLE.COM>
2023-11-19 00:42:43 +0300

    fix px to rem & minor changes

--- src/pages/main/styles.tsx
+++ src/pages/main/styles.tsx
@@ -1,98 +1,94 @@
 import styled from "@emotion/styled";
 
 export const Main = styled.div`
-  max-width: 1920px;
+  max-width: 100rem;
   background-color: #271a58;
-  padding-left: 140px;
-  padding-right: 140px;
-  padding-bottom: 60px;
+  padding-left: 8.75rem;
+  padding-right: 8.75rem;
+  padding-bottom: 3.75rem;
 `;
 
 export const MainHeaderBlock = styled.div`
   display: flex;
-  padding-top: 30px;
-  gap: 864px;
+  padding-top: 1.87rem;
+  gap: 54rem;
 `;
 
 export const SkyfintessLogo = styled.img`
-  width: 220px;
-  height: 35px;
-  color: #ffffff;
+    width: 13.75rem;
+    height: 2.1875rem;
+    flex-shrink: 0;
 `;
 
 export const LoginButton = styled.button`
-  display: inline-flex;
-  width: 77px;
-  height: 36px;
-  padding: 5px 16px 7px 16px;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  border-radius: 46px;
-  background: #140d40;
-  color: #ffffff;
-  text-align: center;
-  font-family: StratosSkyeng, sans-serif;
-  font-variant-numeric: lining-nums proportional-nums;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px; /* 150% */
+    display: inline-flex;
+    padding: 0.3125rem 1rem 0.4375rem 1rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 2.875rem;
+    background: #140D40;
+    width: 4.8rem;
+    height: 2.25rem;
+    color: #ffffff;
+    text-align: center;
+    font-variant-numeric: lining-nums proportional-nums;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; /* 150% */
 
-  &:hover {
-    background: #2d1f79;
-  }
+    &:hover {
+        background: #2d1f79;
+    }
 
-  &:active {
-    background: #3b29a1;
-  }
+    &:active {
+        background: #3b29a1;
+    }
 `;
 
 export const TitleBlock = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-top: 35px;
-  padding-right: 140px;
+  margin-top: 2.19rem;
 `;
 
 export const TitleBlockH3 = styled.h3`
-  color: var(--monochrome-white-20, #fff);
-  font-family: StratosSkyeng, sans-serif;
+  color: #fff;
   font-variant-numeric: lining-nums proportional-nums;
-  font-size: 20px;
+  font-size: 1.25rem;
   font-style: normal;
   font-weight: 400;
   line-height: 115%; /* 23px */
-  letter-spacing: -0.05px;
+  letter-spacing: -0.00313rem;
   opacity: 0.5;
-  margin-bottom: 13px;
+  margin-bottom: 1.06rem;
 `;
 
 export const TitleBlockH1 = styled.h1`
   color: #f4f4ff;
-  font-family: StratosSkyeng, sans-serif;
   font-variant-numeric: lining-nums proportional-nums;
-  font-size: 60px;
+  font-size: 3.75rem;
   font-style: normal;
   line-height: 91.5%; /* 54.9px */
-  letter-spacing: -1.169px;
-  width: 930px;
+  letter-spacing: -0.07306rem;
+  max-width: 51rem;
 `;
 
 export const SkyfintessSlogan = styled.img`
-  width: 220px;
-  height: 180px;
+  width: 15.48rem;
+  height: 15.27rem;
   flex-shrink: 0;
   position: absolute;
-  left: 1160px;
-  top: 120px;
+  right: 4.75rem;
+  top: 6.25rem;
 `;
 
 export const CenterBlock = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 37px;
-  margin-top: 52px;
+  gap: 2.69rem;
+  margin-top: 3.25rem;
+  margin-bottom: 2.12rem;
 `;
 
 export const CourseCard = styled.div`
@@ -100,54 +96,49 @@ export const CourseCard = styled.div`
 `;
 
 export const CourseImage = styled.img`
-  width: 360px;
-  height: 480px;
-  flex-shrink: 0;
-  border-radius: 29.998px;
-  box-shadow: -10px 10px 15.99882px 0px rgba(0, 0, 0, 0.1),
-    10px -10px 16px 0px rgba(0, 0, 0, 0.1);
-  margin-bottom: 44px;
-  position: relative;
+    width: 22.5rem;
+    height: 30rem;
+    flex-shrink: 0;
+    border-radius: 1.875rem;
+    box-shadow: -10px 10px 15.99882px 0px rgba(0, 0, 0, 0.1),
+        10px -10px 16px 0px rgba(0, 0, 0, 0.1);
+    position: relative;
 `;
 
 export const CourseName = styled.h2`
-  width: 290px;
-  height: 120px;
   color: #000;
   font-variant-numeric: lining-nums proportional-nums;
-  font-family: StratosSkyeng, sans-serif;
-  font-size: 36px;
+  font-size: 2.25rem;
   font-style: normal;
   font-weight: 800;
   line-height: 110%; /* 39.6px */
-  letter-spacing: -0.5px;
+  letter-spacing: -0.03125rem;
   position: absolute;
-  top: 30px;
-  left: 30px;
+  top: 1.88rem;
+  left: 1.88rem;
 `;
 
 export const ScrollUpBtn = styled.button`
-  padding: 7px 24px 9px 24px;
-  margin: 0 auto;
-  display: block;
-  border-radius: 46px;
-  background: #c7e957;
-  weigth: 147px;
-  height: 48px;
-  color: #000;
-  font-family: StratosSkyeng, sans-serif;
-  text-align: center;
-  font-variant-numeric: lining-nums proportional-nums;
-  font-size: 24px;
-  font-style: normal;
-  line-height: 32px; /* 133.333% */
-  letter-spacing: -0.1px;
+    padding: 0.4375rem 1.5rem 0.5625rem 1.5rem;
+    margin: 0 auto;
+    display: block;
+    border-radius: 2.875rem;
+    background: #c7e957;
+    weigth: 9.18rem;
+    height: 3rem;
+    color: #000;
+    text-align: center;
+    font-variant-numeric: lining-nums proportional-nums;
+    font-size: 1.5rem;
+    font-style: normal;
+    line-height: 32px; /* 133.333% */
+    letter-spacing: -0.00625rem;
 
-  &:hover {
-    background: #daf289;
-  }
+    &:hover {
+        background: #daf289;
+    }
 
-  &:active {
-    background: #ebffab;
-  }
+    &:active {
+        background: #ebffab;
+    }
 `;
