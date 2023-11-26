58e2c9d28dbeb5b06424d7d2ab99a9ac3c382a08
Arina Gracheva <arinee.msc@gmail.com>
2023-11-26 20:18:39 +0300

    styles

--- src/pages/auth/styles.tsx
+++ src/pages/auth/styles.tsx
@@ -1,2 +1,10 @@
 import styled from '@emotion/styled';
 import { Button } from '../../components/form/Button';
+
+export const PageWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: rgb(208, 206, 206);
+`;
