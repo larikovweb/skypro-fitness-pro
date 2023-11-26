1c4b94262f48d44f6f80af0da5070f17fff13bd0
Arina Gracheva <arinee.msc@gmail.com>
2023-11-26 19:21:44 +0300

    1 commit

--- src/pages/TestLogin.tsx
+++ src/pages/TestLogin.tsx
@@ -1,90 +1,103 @@
 import { FC } from 'react';
-import { Input } from '../components/form/Input';
-import { Button } from '../components/form/Button';
-import { useDispatch } from 'react-redux';
+import { Input } from '../../components/form/Input';
+import { Button } from '../../components/form/Button';
+import { Link } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useForm } from 'react-hook-form';
-import { setUser } from '../store/slices/userSlice';
-import { InputField } from '../components/form/InputField';
+import { InputField } from '../../components/form/InputField';
+import { setUser } from '../../store/slices/userSlice';
+import { useDispatch } from 'react-redux';
+import { HelmetHead } from '../../components/seo/HelmetHead';
+import * as S from './styles';
 
-type TSignUp = {
+type TSignIn = {
   email: string;
   password: string;
-  repeat: string;
 };
 
-const TestLogin: FC = () => {
+const TestLogin: React.FC = () => {
   const dispatch = useDispatch();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<TSignUp>();
+  } = useForm<TSignIn>();
 
-  const onSubmit = (data: TSignUp) => {
+  const onSubmit = async (data: TSignIn) => {
     const auth = getAuth();
-    signInWithEmailAndPassword(auth, data.email, data.password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        dispatch(
-          setUser({
-            email: user.email,
-            id: user.uid,
-            token: user.refreshToken,
-          }),
-        );
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(`${errorCode}: ${errorMessage}`);
-      });
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
+      const user = userCredential.user;
+      dispatch(
+        setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.refreshToken,
+        }),
+      );
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(`${errorCode}: ${errorMessage}`);
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      style={{
-        maxWidth: '17rem',
-        margin: '0 auto',
-      }}
-      action="">
-      <h1>Login</h1>
-      <InputField error={errors.email?.message}>
-        <Input
-          {...register('email', {
-            required: 'Email is required',
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'Некорректный адрес электронной почты',
-            },
-          })}
-          placeholder="Email"
-        />
-      </InputField>
-      <InputField error={errors.password?.message}>
-        <Input
-          {...register('password', {
-            required: 'Password is required',
-            minLength: {
-              value: 6,
-              message: 'Password must be at least 6 characters',
-            },
-            maxLength: {
-              value: 20,
-              message: 'Password must be less than 20 characters',
-            },
-            pattern: {
-              value: /(?=.*[0-9])/,
-              message: 'Password must contain a number',
-            },
-          })}
-          placeholder="Password"
-        />
-      </InputField>
-      <Button $primary>Submit</Button>
-    </form>
+    <>
+      <HelmetHead title="Страница авторизации" descr="Страница авторизации" />
+      <S.Container>
+        <S.CardUser>
+          <S.Logo>
+            <LogoIco fillColor={pathname === '/' ? '#ffffff' : '#140d40'} />
+          </S.Logo>
+          <S.Info>
+            <S.Title>Вход в систему</S.Title>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <InputField error={errors.email?.message}>
+                <Input
+                  {...register('email', {
+                    required: 'Email is required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Некорректный адрес электронной почты',
+                    },
+                  })}
+                  placeholder="Логин"
+                />
+              </InputField>
+              <InputField error={errors.password?.message}>
+                <Input
+                  {...register('password', {
+                    required: 'Password is required',
+                    minLength: {
+                      value: 6,
+                      message: 'Password must be at least 6 characters',
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: 'Password must be less than 20 characters',
+                    },
+                    pattern: {
+                      value: /(?=.*[0-9])/,
+                      message: 'Password must contain a number',
+                    },
+                  })}
+                  placeholder="Пароль"
+                  type="password"
+                />
+              </InputField>
+              <Button $primary type="submit">
+                Войти
+              </Button>
+            </form>
+            <S.RegisterLink>
+              <Link to="'../pages/TestRegister'">Зарегистрироваться</Link>
+            </S.RegisterLink>
+          </S.Info>
+        </S.CardUser>
+      </S.Container>
+    </>
   );
 };
 
