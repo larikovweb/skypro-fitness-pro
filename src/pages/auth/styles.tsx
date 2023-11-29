f81e57c19b4b656434e7fce54f9b1c2958163b6c
larikovweb <larikov.web@gmail.com>
2023-11-29 21:40:39 +0300

    feat: auth page

---
+++ src/pages/auth/styles.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 0.75rem;
+  background: #fff;
+  padding: 2rem 2.6rem 3rem;
+  width: 22.875rem;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+`;
+
+export const Buttons = styled.div`
+  display: grid;
+  gap: 1.25rem;
+  margin-top: 1rem;
+`;
+
+export const Logo = styled.div`
+  margin-bottom: 1.75rem;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
