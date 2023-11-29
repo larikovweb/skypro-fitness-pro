88aafaf42704b0aee3317ec110ec82cd346e7437
Yurii <116386313+larikovweb@users.noreply.github.com>
2023-11-29 21:41:19 +0300

    feat: auth page (#30)

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,16 +3,18 @@ import NotFound from '../pages/NotFound';
 
 import {
   COURSE_ROUTE,
+  LOGIN_ROUTE,
   MAIN_ROUTE,
   NOT_FOUND_ROUTE,
   PROFILE_ROUTE,
+  REGISTER_ROUTE,
   WORKOUT_ROUTE,
 } from '../utils/consts';
 import Course from '../pages/course/Course';
-import TestLogin from '../pages/TestLogin';
-import TestRegister from '../pages/TestRegister';
 import Profile from '../pages/profile/Profile';
 import { Workout } from '../pages/workout/Workout';
+import Login from '../pages/auth/Login';
+import Register from '../pages/auth/Register';
 
 export type RouteType = {
   path: string;
@@ -22,8 +24,8 @@ export type RouteType = {
 export const publicRoutes: RouteType[] = [
   { path: MAIN_ROUTE, component: <Main /> },
   { path: `${COURSE_ROUTE}/:id`, component: <Course /> },
-  { path: '/test-login', component: <TestLogin /> },
-  { path: '/test-register', component: <TestRegister /> },
+  { path: LOGIN_ROUTE, component: <Login /> },
+  { path: REGISTER_ROUTE, component: <Register /> },
 
   //...
   { path: NOT_FOUND_ROUTE, component: <NotFound /> },
