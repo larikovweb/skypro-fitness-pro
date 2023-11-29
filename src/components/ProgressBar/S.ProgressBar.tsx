c4ce339add7a8b71e1b41a782e21045f30941f7b
larikovweb <larikov.web@gmail.com>
2023-11-29 11:43:14 +0300

    feat: workout page

--- src/components/ProgressBar/S.ProgressBar.tsx
+++ src/components/ProgressBar/S.ProgressBar.tsx
@@ -1,53 +1,46 @@
 import styled from '@emotion/styled';
 
 interface Props {
-  progress?: number | null;
+  progress?: number | string | null;
   bgcolor?: string;
 }
 
-export const ProgressBlock = styled.div`
+export const Wrapper = styled.div`
   border-radius: 1.875rem;
   background: #f2f2f2;
   flex-direction: column;
+  padding: 2.25rem 2.75rem 2rem;
 `;
+
 export const Title = styled.p`
-  padding: 2.5rem 5rem;
-  text-align: center;
-  color: #000;
-  font-variant-numeric: lining-nums proportional-nums;
   font-size: 2rem;
   font-style: normal;
   font-weight: 400;
-  line-height: 2.5rem; /* 125% */
-`;
-export const ProgressMain = styled.div`
-  display: flex;
-  padding: 0 2.44rem 1.62rem 2.75rem;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+  line-height: 2.5rem;
+  text-align: center;
+  margin-bottom: 2.5rem;
 `;
 
-export const TextBlockP = styled.p`
-  color: #000;
-  font-size: 1.5rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 2rem; /* 133.333% */
+export const Bars = styled.div`
+  display: flex;
+  flex-direction: column;
+  > *:not(:last-child) {
+    margin-bottom: 1.63rem;
+  }
 `;
 
 export const Block = styled.div<Props>`
   width: 100%;
   background-color: #edecff;
-  border-radius: 50px;
-  margin: 50px;
+  border-radius: 1.375rem;
   overflow: hidden;
-  border: 2px solid ${({ bgcolor }) => bgcolor};
+  border: 0.125rem solid ${({ bgcolor }) => bgcolor};
 `;
 
 export const Result = styled.div<Props>`
   width: ${({ progress }) => (progress ? `${progress}%` : `100%`)};
   background-color: ${({ progress, bgcolor }) => (progress ? bgcolor : `#fff`)};
+  transition: width 0.3s, background-color 0.3s;
 `;
 
 export const Label = styled.span<Props>`
@@ -62,11 +55,4 @@ export const Label = styled.span<Props>`
   flex-direction: row;
 
   ${({ progress }) => (progress ? `justify-content: flex-end;` : `justify-content:center`)};
-`;
-
-export const TextBlock = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
 `;
--- src/pages/workout/Workout.tsx
+++ src/pages/workout/Workout.tsx
@@ -3,30 +3,14 @@ import { useParams } from 'react-router-dom';
 import { useUserData } from '../../hooks/useUserData';
 import { Loader } from '../../components/plug/Loader';
 import { Container } from '../../styled/components';
-import { useForm } from 'react-hook-form';
-import { IExercise } from '../../interfaces/interfaces';
-import { Input } from '../../components/form/Input';
-import { InputField } from '../../components/form/InputField';
-import { fetchUserData, saveExerciseReps } from '../../services/userDataService';
-import { useAuth } from '../../hooks/useAuth';
-import { useAppDispatch } from '../../store/store';
 import { ProgressBar } from '../../components/ProgressBar/ProgressBar';
-import * as S from '../../components/ProgressBar/S.ProgressBar';
+import { Button } from '../../components/form/Button';
+import * as S from './styles';
+import { ModalControl } from '../../components/modals/ModalControl';
+import { ModalWorkout } from '../../components/modals/ModalWorkout';
 
-type Form = {
-  [key: string]: IExercise;
-};
-
-export const TestWorkout: FC = () => {
-  const { id: userId } = useAuth();
-  const dispatch = useAppDispatch();
+export const Workout: FC = () => {
   const { courseId, workoutId } = useParams();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<Form>();
 
   const { courses, status, error } = useUserData();
 
@@ -42,77 +26,44 @@ export const TestWorkout: FC = () => {
     return <div>{error}</div>;
   }
 
-  const onSubmit = (data: Form) => {
-    if (courseId && userId && workoutId && workout) {
-      saveExerciseReps(userId, courseId, workoutId, data)
-        .then(() => {
-          console.log('Saved exercise reps successfully');
-          userId && dispatch(fetchUserData(userId));
-          reset();
-        })
-        .catch((error) => {
-          console.error('Failed to save exercise reps:', error);
-        });
-    } else {
-      alert('ids not found');
-    }
-  };
+  function getYoutubeEmbedUrl(url: string | undefined) {
+    const videoId = url && url.split('https://youtu.be/')[1];
+    return 'https://www.youtube.com/embed/' + videoId;
+  }
 
-  /*массив цветов шкалы прогресса*/
-  const progressData = [
-    { bgcolor: '#565EEF' },
-    { bgcolor: '#FF6D00' },
-    { bgcolor: '#9A48F1' },
-    { bgcolor: '#565EEF' },
-  ];
+  const title = workout?.title.split(' / ')[0];
+  const descr = workout?.title.split(' / ').slice(1).join(' / ');
+
   return (
     <Container>
-      {JSON.stringify(workout)}
-      <S.Title>{workout?.title}</S.Title>
-      <S.ProgressBlock>
-        <S.Title> {workout?.title} </S.Title>
-        {workout?.exercises.map((item, i) => (
-          <>
-            <S.ProgressMain>
-              <S.TextBlockP> {item.name} </S.TextBlockP>
-              <ProgressBar
-                key={i}
-                bgcolor={progressData[i].bgcolor}
-                completed={item.myReps}
-                max_completed={item.reps}
-              />
-            </S.ProgressMain>
-          </>
-        ))}
-      </S.ProgressBlock>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {workout?.exercises.map((item, i) => (
-          <div key={i}>
-            <label htmlFor={`exercises.${i}.myReps`}>{item.name}</label>
-            <InputField error={errors[i]?.myReps?.message}>
-              <Input
-                type="number"
-                id={`exercises.${i}.myReps`}
-                {...register(`${i}.myReps`, {
-                  required: {
-                    value: true,
-                    message: 'Required',
-                  },
-                  max: {
-                    value: item.reps,
-                    message: `Cannot exceed maximum reps of ${item.reps}`,
-                  },
-                  min: {
-                    value: 0,
-                    message: `Cannot be less than 0`,
-                  },
-                })}
-              />
-            </InputField>
-          </div>
-        ))}
-        <button type="submit">Submit</button>
-      </form>
+      <S.Title>{title}</S.Title>
+      <S.Descr>{descr}</S.Descr>
+      <S.Video>
+        <iframe
+          src={getYoutubeEmbedUrl(workout?.url)}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </S.Video>
+      <S.Wrapper>
+        <S.Block>
+          <S.Label>Упражнения</S.Label>
+          <S.List>
+            {workout?.exercises.map((item, i) => (
+              <li key={i}>
+                {item.name} ({item.reps} повторений)
+              </li>
+            ))}
+          </S.List>
+          <ModalControl
+            id={`workout-${courseId}-${workoutId}`}
+            modal={<ModalWorkout courseId={courseId} workoutId={workoutId} workout={workout} />}>
+            <Button $primary>Заполнить свой прогресс</Button>
+          </ModalControl>
+        </S.Block>
+        {workout && <ProgressBar workoutId={workoutId} exercises={workout?.exercises} />}
+      </S.Wrapper>
     </Container>
   );
 };
